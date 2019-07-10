ad21441da1238b153b913712d4110c3f91a75401
Armando Carballo <armando@Armandos-MacBook-Pro.local>
2019-07-09 21:40:05 -0600

    Created interfaces for models. Closes 5.

---
+++ pet-clinic-data/src/main/java/com/jcarbad/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.jcarbad.sfgpetclinic.services;
+
+import com.jcarbad.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
