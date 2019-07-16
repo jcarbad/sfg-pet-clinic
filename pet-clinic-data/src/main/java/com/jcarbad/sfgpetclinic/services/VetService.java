0ebd8ef7419bcae44ad9db2ab10cddc88cb50251
Armando Carballo <joan.carballo@homeadvisor.com>
2019-07-15 19:52:22 -0600

    Added CrudService common interface. CLoses #11

--- pet-clinic-data/src/main/java/com/jcarbad/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/jcarbad/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.jcarbad.sfgpetclinic.services;
 
 import com.jcarbad.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
