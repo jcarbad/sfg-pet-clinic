d3e01425b71d6218cc0394f1dba60d3cd38937af
Armando Carballo <joan.carballo@homeadvisor.com>
2019-11-10 20:55:48 -0600

    Created Pets JPA service

---
+++ pet-clinic-data/src/main/java/com/jcarbad/sfgpetclinic/services/datajpa/PetJpaService.java
@@ -0,0 +1,47 @@
+package com.jcarbad.sfgpetclinic.services.datajpa;
+
+import com.jcarbad.sfgpetclinic.model.Pet;
+import com.jcarbad.sfgpetclinic.repositories.PetRepository;
+import com.jcarbad.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class PetJpaService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
