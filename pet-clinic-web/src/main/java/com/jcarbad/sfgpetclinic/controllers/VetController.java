3e866fb1e7d7943aeef3fa5682b27bc7a0403621
Armando Carballo <joan.carballo@homeadvisor.com>
2019-12-30 22:17:37 -0600

    Added Vets list JSON endpoint.Closes #39

--- pet-clinic-web/src/main/java/com/jcarbad/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/jcarbad/sfgpetclinic/controllers/VetController.java
@@ -1,12 +1,16 @@
 package com.jcarbad.sfgpetclinic.controllers;
 
+import com.jcarbad.sfgpetclinic.model.Vet;
 import com.jcarbad.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
-@RequestMapping("/vets")
 public class VetController {
 
     private final VetService vetService;
@@ -15,11 +19,17 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/index", "/list"})
+    @GetMapping({"/vets", "vets/", "/index", "/list", "/vets.html"})
     public String listVets (Model model) {
 
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/vets")
+    public Set<Vet> getJsonVets() {
+        return vetService.findAll();
     }
 }
